7c5a9f975b681b3b41e302e45ec70f4972c05985
Stefan <stefan_saramet@yahoo.com>
2021-11-10 12:55:22 +0200

    Add controller for otp generation

---
+++ OTP.Challenge/Controllers/OtpController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OTP.Challenge.Models;
+
+namespace OTP.Challenge.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OtpController : ControllerBase
+{
+    public OtpController()
+    {
+
+    }
+
+    [HttpPost("generate")]
+    public GenerateOtpResponseModel GenerateOtp([FromBody] GenerateOtpRequestModel otpRequestModel)
+    {
+        //generate new otp
+        //start quartz job
+        //persist otp
+        return new GenerateOtpResponseModel();
+    }
+
+    [HttpPost("stop/{userId}")]
+    public void StopOtpGeneration(Guid userId)
+    {
+        //stop quartz job
+        //delete otp for userid
+    }
+}
