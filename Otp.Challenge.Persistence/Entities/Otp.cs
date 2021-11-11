33c532c7b0985ef377169343b15ac8f1d24ab265
Stefan <stefan_saramet@yahoo.com>
2021-11-11 03:11:52 +0200

    Remove persistence files

--- Otp.Challenge.Persistence/Entities/Otp.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Otp.Challenge.Persistence.Entities;
-
-public class Otp
-{
-    public Guid UserId { get; set; }
-    public string Password { get; set; }
-    public TimeOnly ValidUntil { get; set; }
-}
